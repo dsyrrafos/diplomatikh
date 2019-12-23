5dfe41aa67a3321ef179698e9f68cf5440eb3880
dsyrrafos <49962629+dsyrrafos@users.noreply.github.com>
2019-12-23 18:22:14 +0200

    Add files via upload

---
+++ enc_dec.py
@@ -0,0 +1,147 @@
+from keras.models import Model, load_model
+from keras.layers import Dense, LSTM, Input, Bidirectional, Concatenate, dot, Activation, TimeDistributed
+from keras.callbacks import TensorBoard
+from keras.utils import plot_model
+from sklearn.model_selection import train_test_split
+from attention import AttentionLayer
+from keras import backend as k
+#from custom_recurrents import AttentionDecoder
+import numpy as np
+import sys
+import time
+
+
+def create_model(latent_dim, bidirectional):
+	attention = False
+	## Create encoder
+	encoder_inputs = Input(shape=(None, 14))
+
+	if bidirectional:
+		encoder = Bidirectional(LSTM(latent_dim, return_state = True))
+		encoder_outputs, forward_h, forward_c, backward_h, backward_c = encoder(encoder_inputs)
+		state_h = Concatenate()([forward_h, backward_h])
+		state_c = Concatenate()([forward_c, backward_c])
+		print('state_h.size', k.shape(state_h))
+		print('state_c.size', k.shape(state_c))
+	else:
+		encoder = LSTM(latent_dim, return_sequences=True, return_state = True)#
+		#encoder = LSTM(latent_dim, return_state = True)
+		encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+	
+	encoder_states = [state_h, state_c]
+	## Keep encoder as separate model
+	enc_model = Model(encoder_inputs,encoder_states,name='encoder_model')
+
+
+	## Create decoder
+	decoder_input = Input(shape=(None, 38))
+	if bidirectional:
+		in_h_state = Input(shape=(2*latent_dim,))
+		in_e_state = Input(shape=(2*latent_dim,))
+		decoder_lstm = LSTM(latent_dim*2, return_sequences = True, return_state = True)
+	else:
+		in_h_state = Input(shape=(latent_dim,))
+		in_e_state = Input(shape=(latent_dim,))
+		decoder_lstm = LSTM(latent_dim, return_sequences = True, return_state = True)
+	decoder_outputs, dec_h_state, dec_c_state = decoder_lstm(decoder_input, initial_state = [in_h_state,in_e_state])
+
+	if attention:
+		#---------------------------------------------------------------------------------------------------------------------
+		# Attention layer
+		attn_layer = AttentionLayer(name='attention_layer')
+		attn_out, attn_states = attn_layer([encoder_outputs, decoder_outputs])
+
+		# Concat attention input and decoder GRU output
+		decoder_concat_input = Concatenate(axis=-1, name='concat_layer')([decoder_outputs, attn_out])
+		#---------------------------------------------------------------------------------------------------------------------
+
+	decoder_dense = Dense(38,activation='softmax')
+	decoder_outputs = decoder_dense(decoder_outputs)
+	## Keep decoder as separate model
+	dec_model = Model([decoder_input,in_h_state,in_e_state],[decoder_outputs,dec_h_state,dec_c_state],name='decoder_model')
+
+	## Combined encoder and decoder model
+	## Inputs are encoder input and decoder input
+	## Output is the 0th output of dec_model (not the states) when applied on decoder input with encoded states
+	model = Model([encoder_inputs,decoder_input],dec_model([decoder_input]+enc_model(encoder_inputs))[0],name='combined_model')
+	model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+	
+	## Return all 3 models
+	model.summary()
+	#input('pause')
+	return enc_model,dec_model,model
+
+
+def train():
+	max_len = 128
+	batch_size = 64
+	epochs = 300
+	latent_dim = 256
+	bidirectional = False
+	np.set_printoptions(threshold=sys.maxsize)
+	np.set_printoptions(suppress=True)
+	path = '../../../../data/data1/users/dsyrrafos/seq2seq/dataset/'
+
+	#encoder_input_data = np.load(path+'encoder_input_data.npy')[:,:max_len,:]
+	#decoder_input_data = np.load(path+'decoder_input_data.npy')[:,:max_len,:]
+	#decoder_target_data = np.load(path+'decoder_target_data.npy')[:,:max_len,:]
+	
+	encoder_input_data = np.load(path+'small_encoder_input_data.npy')
+	decoder_input_data = np.load(path+'small_decoder_input_data.npy')
+	decoder_target_data = np.load(path+'small_decoder_target_data.npy')
+
+	path = '../../../../data/data1/users/dsyrrafos/seq2seq/logs/'
+	encoder,decoder,combined = create_model(latent_dim, bidirectional)
+	tensorboard = TensorBoard(log_dir=path+"logs/{}".format(time.time())+'_'+str(epochs))
+
+	combined.fit([encoder_input_data, decoder_input_data], decoder_target_data, batch_size=batch_size, epochs=epochs, validation_split=0.2, callbacks=[tensorboard])
+
+	path = '../../../../data/data1/users/dsyrrafos/seq2seq/models/'
+	encoder.save(path+'encoder.h5')
+	decoder.save(path+'decoder.h5')
+	combined.save(path+'combined.h5')
+
+
+def predict():	
+	max_len = 128
+
+	test_song = 4
+	path = '../../../../data/data1/users/dsyrrafos/seq2seq/models/eddie/'
+	encoder = load_model(path+'encoder.h5')
+	decoder = load_model(path+'decoder.h5')
+
+	encoder.summary()
+	decoder.summary()
+	path = '../../../../data/data1/users/dsyrrafos/seq2seq/dataset/'
+	encoder_input_data = np.load(path+'test_encoder_input_data.npy')[test_song:test_song+1,:max_len,:]
+	decoder_input_data = np.load(path+'test_decoder_input_data.npy')[test_song:test_song+1,:max_len,:]
+	decoder_target_data = np.load(path+'test_decoder_target_data.npy')[test_song:test_song+1,:max_len,:]
+
+	enc_h,enc_c = encoder.predict(encoder_input_data)
+	target_seq = np.zeros((1, 1, 38))
+	target_seq[0,0,36] = 1
+
+	o_token,h,c = decoder.predict([target_seq,enc_h,enc_c])
+	
+	target_seq=np.concatenate([target_seq,o_token],axis=1)
+
+	for i in range(max_len):
+		print(i)
+		o_token,h,c = decoder.predict([o_token,h,c])
+		target_seq=np.concatenate([target_seq,o_token],axis=1)
+
+	print('==========================================')
+	print('NOTES: ')
+	print(np.argmax(encoder_input_data[0],axis=-1))
+	print("PREDICTED CHORDS : ")
+	print(np.argmax(target_seq[0],axis=-1))
+	print("REAL CHORDS")
+	print(np.argmax(decoder_target_data[0],axis=-1))
+	#input('next')
+	print('==========================================')
+
+
+if __name__ == "__main__":
+	#create_model(256)
+	train()
+	#predict()
